484236da801233ddcb7fd3edc0fc1e469f61f9c1
Erlenilde Santos <erleliu@yahoo.com.br>
2025-04-10 11:27:22 -0300

    team_eternalquest

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,25 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) : base(name, description, points)
+    {
+        
+    }
+
+    public override void RecordEvent()
+    {
+        
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+
+}
